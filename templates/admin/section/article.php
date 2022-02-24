572223136bf2df54dfe0e06bf491b76c254a3143
57-house <njiegnamnjuhdonald@gmail.com>
2022-02-24 21:51:09 +0100

    article-listing

--- templates/admin/section/article.php
+++ templates/admin/section/article.php
@@ -11,7 +11,42 @@
 
 ?>
 <div class="container-fluid scd_boder">
-	<form action="options.php" method="post" id="general_option_form" name="general_option_form">
-		how they know ?
-	</form>
+	<?php
+	if ( $articles->have_posts() ) :
+		while ( $articles->have_posts() ) :
+			$articles->the_post();
+			?>
+			<form action="#" method="POST">
+				<div class="card-body row p-0  mx-0 w-100 justify-content-between text-dark">
+					<small for="" class="col-form-label mx-0 px-0"><strong>Titre : <?php echo esc_html( the_title() ); ?></strong></small>
+					<div class="form-group row w-100 mx-0 mb-0">
+						<label for="" class="col-form-label mx-0 px-0"><strong>Price (<?php echo esc_html( get_woocommerce_currency_symbol() ); ?>) </strong> :</label>
+						<div class="col-sm-10 px-0 mx-1">
+							<input type="text" class="form-control shadow-sm rounded-0" id="" placeholder="Mettez un prix pour vendre cette article." name="article_price[]" value="<?php echo esc_html( get_post_meta( get_the_ID(), '_article_product_price', true ) ?? '' ); ?>">
+							<input type="hidden" class="form-control shadow-sm rounded-0" id="" name="article_id[]" value="<?php echo esc_html( get_the_ID() ); ?>">
+							<input type="hidden" class="form-control shadow-sm rounded-0" id="" name="article_name[]" value="<?php echo esc_html( the_title() ); ?>">
+							<input type="hidden" class="form-control shadow-sm rounded-0" id="" name="article_description[]" value="<?php echo esc_html( the_title() ); ?>">
+						</div>
+					</div>
+					<p class="card-text"><small class="text-muted"><strong><?php echo esc_html( get_the_date( 'Y-m-d' ) ); ?></strong></small></p>
+				</div>
+				<hr>
+			<?php
+		endwhile;
+	endif;
+	?>
+		<ul class="text-dark d-flex row w-100 mx-0">
+			<?php foreach ( $page_links as $ctp_page_link ) : ?>
+				<li class="mx-2">
+					<?php if ( isset( $_GET['paged'] ) && $ctp_page_link->page === $_GET['paged'] ) : ?>
+						<strong><?php esc_html( $ctp_page_link->page ); ?></strong>
+					<?php else : ?>
+						<a href="<?php esc_attr( $ctp_page_link->url ); ?>">
+							<?php echo esc_html( $ctp_page_link->page ); ?>
+						</a>
+					<?php endif; ?>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+			</form>
 </div>
